350f43deeb5a7d928a6f6849088b56afe8b52b24
Bratha <119675494+BREGASOV1@users.noreply.github.com>
2025-11-28 00:29:25 +0300

    Initial commit - DVLD

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database\DVLD_Template.mdf;Integrated Security=True;";
+
+
+    }
+}
